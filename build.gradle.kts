d14af9870115ea6a089228e9e8d75e49201bf715
Dyablouz <fernando123lawrence@gmail.com>
2026-02-20 19:24:21 +0700

    feat: add CD/CD

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     java
     id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "7.2.0.6526"
+    jacoco
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -17,6 +19,26 @@ java {
 configurations {
     compileOnly {
         extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "advprog-2026-A18-project_Manajemen-Pembayaran")
+        property("sonar.organization", "advprog-2026-a18-project")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
     }
 }
 
