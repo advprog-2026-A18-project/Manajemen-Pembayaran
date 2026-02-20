25b9c39c77034cc09a4342f0e7ef5ed22baf3cde
Dyablouz <fernando123lawrence@gmail.com>
2026-02-20 10:29:39 +0700

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/manajemenpembayaran/ManajemenPembayaranApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.manajemenpembayaran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManajemenPembayaranApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManajemenPembayaranApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/manajemenpembayaran/ManajemenPembayaranApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.manajemenpembayaran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManajemenPembayaranApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
