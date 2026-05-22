7c20b80a6f5cbc67e35a54f58210a3667b0d5699
Dyablouz <fernando123lawrence@gmail.com>
2026-05-22 07:43:26 +0700

    Revert "[GREEN]: add payment monitoring endpoints"
    This reverts commit a54834ccc63f110cc98dc21a39c5bdcdcc306714.

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,8 +54,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("me.paulschwarz:spring-dotenv:$dotenvVersion")
     implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
     compileOnly("org.projectlombok:lombok")
